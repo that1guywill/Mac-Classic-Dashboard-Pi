54d91910b879f60595b9fcecdb09e4db4c27787b
that1guywill <mrwill712@gmail.com>
2022-07-22 03:49:39 +0100

    added date to widgets

---
+++ src/script.js
@@ -0,0 +1,19 @@
+function updateDate() {
+    dt = new Date();
+
+    function english_ordinal_suffix(dt)
+    {
+        return dt.getDate()+(dt.getDate() % 10 == 1 && dt.getDate() != 11 ? 'st' : (dt.getDate() % 10 == 2 && dt.getDate() != 12 ? 'nd' : (dt.getDate() % 10 == 3 && dt.getDate() != 13 ? 'rd' : 'th')));
+    }
+
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    let month = monthNames[dt.getMonth()];
+    let year = dt.getFullYear();
+
+    document.getElementById('date').innerHTML = (english_ordinal_suffix(dt) + ' of ' + month + ', ' + year) ;
+}
+
+updateDate();
